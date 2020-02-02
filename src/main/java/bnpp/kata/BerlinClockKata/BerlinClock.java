931127a2b69cee96bd05fb82d4a985b5284023da
aaditya2290 <aaditya_2290@yahoo.co.in>
2020-02-02 18:56:35 +0530

    verifying seconds to Berlin Clock seconds lamp conversion.

--- src/main/java/bnpp/kata/BerlinClockKata/BerlinClock.java
+++ src/main/java/bnpp/kata/BerlinClockKata/BerlinClock.java
@@ -2,4 +2,9 @@ package bnpp.kata.BerlinClockKata;
 
 public class BerlinClock {
 
+	public String convertSecondsToSecondsLamp(int seconds)
+	{
+		return (seconds%2==0?"Y":"O");
+	}
+
 }
--- src/test/java/bnpp/kata/BerlinClockKata/BerlinClockTest.java
+++ src/test/java/bnpp/kata/BerlinClockKata/BerlinClockTest.java
@@ -1,6 +1,9 @@
 package bnpp.kata.BerlinClockKata;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class BerlinClockTest {
 
@@ -9,6 +12,12 @@ public class BerlinClockTest {
 	@Before
 	public void InitializeBerlinClock() {
 		clock=new BerlinClock();
+	}
+
+	@Test
+	public void testSecondsToSecondsLampConversion(){
+		assertEquals("Y",clock.convertSecondsToSecondsLamp(10));
+		assertEquals("O",clock.convertSecondsToSecondsLamp(7));
 	}
 
 }
